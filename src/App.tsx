4cdf169e8b1844dcf0005563e5576479a67747ae
Fajar Maulana <fajarma21@gmail.com>
2025-09-01 17:43:47 +0700

    fix(build): fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Dialog } from '../lib/main';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   const [display, setDisplay] = useState(false);
