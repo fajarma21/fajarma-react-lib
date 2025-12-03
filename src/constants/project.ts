234bdbf4a4ee8b8ea13a796d77450f83c1d528fe
Fajar Maulana <fajarma21@gmail.com>
2025-12-03 18:19:02 +0700

    feat(projectCard): add testing component

---
+++ src/constants/project.ts
@@ -0,0 +1,22 @@
+export const DUMMY_PROJECT_CARD = {
+  id: 1,
+  title: 'Fajarma Resume',
+  url: 'https://fajarma.com',
+  desktopOnly: false,
+  repo: ['https://fajarma.com'],
+  stacks: [
+    '{"name": "React", "url": "https://react.dev/"}',
+    '{"name": "Typescript", "url": "https://www.typescriptlang.org/"}',
+    '{"name": "Zustand", "url": "https://zustand-demo.pmnd.rs/"}',
+    '{"name": "Firebase", "url": "https://firebase.google.com/docs/firestore"}',
+  ],
+  images: [
+    'https://wkkfnusxohjljpjotkcv.supabase.co/storage/v1/object/public/resume/resume-1.png',
+    'https://wkkfnusxohjljpjotkcv.supabase.co/storage/v1/object/public/resume/resume-2.png',
+  ],
+  thumbnail:
+    'https://wkkfnusxohjljpjotkcv.supabase.co/storage/v1/object/public/common/fajarma-long-thumb.png',
+  videos:
+    'https://wkkfnusxohjljpjotkcv.supabase.co/storage/v1/object/public/tic%20tac%20toe/tic-tac-toe-video-1.mp4',
+  desc: 'My professional resume website.',
+};
